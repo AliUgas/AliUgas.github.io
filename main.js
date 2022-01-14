944dd9747598cbf86445974966ad701b172c20f0
Ali Ugas <alibileugas@gmail.com>
2022-01-14 13:32:03 -0800

    initial animation

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./flappy.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
+	gameEngine.addEntity(new Flappy(gameEngine));
 	gameEngine.init(ctx);
+
 
 	gameEngine.start();
 });
